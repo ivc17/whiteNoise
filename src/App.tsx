35c4f6821f613e6180bda3b3d23366ffe9aad366
ivc17 <ivc1741@gmail.com>
2022-09-22 18:11:10 +0800

    add icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import Canvas from './components/Canvas'
+import { ReactComponent as Icon } from './assets/icon.svg'
 
 function App() {
   return (
     <>
+      <div style={{ position: 'relative' }}>
+        <a
+          href="https://ivc17.github.io/gallery/white_noise"
+          className="icon"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icon />
+        </a>
+      </div>
       <div className="title">WHITE_NOISE</div>
       <Canvas></Canvas>
     </>
